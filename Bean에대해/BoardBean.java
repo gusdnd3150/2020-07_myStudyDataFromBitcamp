f4e56753f43325941124ca490ceb1ce5630f5d44
gusdnd3150 <66711577+gusdnd3150@users.noreply.github.com>
2020-07-17 14:20:49 +0900

    Add files via upload

---
+++ Bean에대해/BoardBean.java
@@ -0,0 +1,34 @@
+package beanTest;
+
+public class BoardBean {
+	private String id;
+	private String pwd;
+	private String title;
+	private String content;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+
+}
